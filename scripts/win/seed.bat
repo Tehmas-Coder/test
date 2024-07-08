f02132ebdd7600b29d34ed0064a397ae5c2d247b
Rana Naeem Afzal <44816058+r-naeem-afzal@users.noreply.github.com>
2024-07-08 12:33:17 +0500

    added tag seeds

--- scripts/win/seed.bat
+++ scripts/win/seed.bat
@@ -3,3 +3,4 @@ REM -------------------------------- LOOKUPS -----------------------------------
 python manage.py loaddata media_type_seed.json
 python manage.py loaddata measuring_unit_seed.json
 python manage.py populate_countries_and_regions_and_currencies
+python manage.py loaddata tag_seed.json
