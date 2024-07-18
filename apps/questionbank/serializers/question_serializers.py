1c3ec30952a53dc84204129566fded4dbe941990
Rana Naeem Afzal <44816058+r-naeem-afzal@users.noreply.github.com>
2024-07-18 17:35:02 +0500

    patch question serializer stable

--- apps/questionbank/serializers/question_serializers.py
+++ apps/questionbank/serializers/question_serializers.py
@@ -17,7 +17,7 @@ from apps.questionbank.serializers.question_subject_serializers import (
     QuestionSubjectDetailSerializer,
     QuestionSubjectEditSerializer,
 )
-from utils.rna_utils import debug_print
+from utils.rna_utils import color_print, debug_print
 from apps.questionbank.serializers.question_choice_serializers import (
     QuestionChoiceEditSerializer,
 )
@@ -58,14 +58,7 @@ class QuestionDetailSerializer(BaseModelSerializer):
 
 
 class QuestionEditSerializer(serializers.ModelSerializer):
-    question_subjects = QuestionSubjectEditSerializer(many=True)
-    # tags = serializers.PrimaryKeyRelatedField(
-    #     queryset=Tag.objects.all(), many=True, required=False
-    # )
-    # choices = QuestionChoiceEditSerializer(many=True, required=False)
-    # attempt_responses = QuestionAttemptResponseEditSerializer(many=True, required=False)
-    # retry_hints = QuestionRetryHintEditSerializer(many=True, required=False)
-    # medias = MediaSerializer(many=True, required=False)
+    subjects = QuestionSubjectEditSerializer(many=True)
 
     class Meta:
         model = Question
@@ -77,29 +70,18 @@ class QuestionEditSerializer(serializers.ModelSerializer):
             "retry_penalty",
             "can_shuffle",
             "has_media",
-            "question_subjects",
-            # "tags",
-            # "choices",
-            # "attempt_responses",
-            # "retry_hints",
-            # "medias",
+            "subjects",
         ]
 
         read_only_fields = ["id"]
 
     def create(self, validated_data):
-
-        question_subjects_data = validated_data.pop("question_subjects")
-        # question_medias = validated_data.pop("medias", [])
-        # tags_data = validated_data.pop("tags", [])
-        # choices_data = validated_data.pop("choices", [])
-        # attempt_responses_data = validated_data.pop("attempt_responses", [])
-        # retry_hints_data = validated_data.pop("retry_hints", [])
+        subjects_data = validated_data.pop("subjects")
 
         # * Create question
         question = Question.objects.create(**validated_data)
 
-        for question_subject_data in question_subjects_data:
+        for question_subject_data in subjects_data:
             question_subject_countries = question_subject_data.pop("countries")
             subject_education_level_data = question_subject_data.pop(
                 "subject_education_level"
@@ -121,39 +103,49 @@ class QuestionEditSerializer(serializers.ModelSerializer):
             # * Assign countries to question subject
             question_subject.countries.set(question_subject_countries)
 
-        # # * create choices
-        # for choice_data in choices_data:
-        #     choices_media = choice_data.pop("medias", [])
-        #     question_choice_instance = QuestionChoice.objects.create(
-        #         question=question, **choice_data
-        #     )
-        #     for media_data in choices_media:
-        #         media_instance = MediaSerializer().create(media_data)
-        #         question_choice_instance.medias.add(media_instance)
-
-        # # * create attempt responses
-        # for attempt_response_data in attempt_responses_data:
-        #     QuestionAttemptResponse.objects.bulk_create(
-        #         [QuestionAttemptResponse(question=question, **attempt_response_data)]
-        #     )
-
-        # # * create retry hints
-        # for retry_hint_data in retry_hints_data:
-        #     retry_hints_media = retry_hint_data.pop("medias", [])
-
-        #     question_hint_instance = QuestionRetryHint.objects.create(
-        #         question=question, **retry_hint_data
-        #     )
-        #     for media_data in retry_hints_media:
-        #         media_instance = MediaSerializer().create(media_data)
-        #         question_hint_instance.medias.add(media_instance)
-
-        # # * Assign tags
-        # question.tags.set(tags_data)
-
-        # # * Upload Media
-        # for media_data in question_medias:
-        #     media_instance = MediaSerializer().create(media_data)
-        #     question.medias.add(media_instance)
-
         return question
+
+    def update(self, instance, validated_data):
+        debug_print(validated_data)
+        subjects_data = validated_data.pop("subjects")
+
+        # * Update question
+        instance.title = validated_data.get("title", instance.title)
+        instance.text = validated_data.get("text", instance.text)
+        instance.max_retries = validated_data.get("max_retries", instance.max_retries)
+        instance.retry_penalty = validated_data.get(
+            "retry_penalty", instance.retry_penalty
+        )
+        instance.can_shuffle = validated_data.get("can_shuffle", instance.can_shuffle)
+        instance.has_media = validated_data.get("has_media", instance.has_media)
+        instance.save()
+
+        # * Update or create question subjects
+        for question_subject_data in subjects_data:
+            question_subject_countries = question_subject_data.pop("countries")
+            subject_education_level_data = question_subject_data.pop(
+                "subject_education_level"
+            )
+
+            # * Get or create subject education level
+            subject_education_level, _ = SubjectEducationLevel.objects.get_or_create(
+                subject=subject_education_level_data["subject"],
+                education_level=subject_education_level_data["education_level"],
+            )
+
+            debug_print(subject_education_level.__dict__)
+
+            # * Get or create question subject
+            question_subject, _ = QuestionSubject.objects.get_or_create(
+                question=instance,
+                subject_education_level=subject_education_level,
+                defaults=question_subject_data,
+            )
+
+            # * Update question subject
+            question_subject.save()
+
+            # * Assign countries to question subject
+            question_subject.countries.set(question_subject_countries)
+
+        return instance
--- apps/questionbank/views.py
+++ apps/questionbank/views.py
@@ -24,7 +24,7 @@ from rest_framework.decorators import action
 from django.db.models import Prefetch
 from apps.questionbank.utils.question_utils import get_question_detail_queryset
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
-from utils.rna_utils import debug_print
+from utils.rna_utils import color_print, debug_print
 
 
 class EducationLevelViewSet(viewsets.ModelViewSet):
@@ -45,66 +45,27 @@ class SubjectEducationLevelViewSet(viewsets.ModelViewSet):
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = get_question_detail_queryset()
     serializer_class = QuestionDetailSerializer
-    parser_classes = (MultiPartParser, FormParser, JSONParser)
-
-    def parse_media(self, request):
-        request_data = json.loads(request.data["data"])
-
-        # Extract media for questions
-        media_keys = request_data.pop("medias", [])
-        request_data["medias"] = []
-        for key in media_keys:
-            file = request.FILES.get(key)
-            if file:
-                request_data["medias"].append(
-                    {
-                        "file": file,
-                    }
-                )
-
-        # Extract media for hints
-        for hint in request_data["retry_hints"]:
-            hint_medias = hint.pop("medias", [])
-            if hint["has_media"]:
-                hint["medias"] = []
-                for key in hint_medias:
-                    file = request.FILES.get(key)
-                    if file:
-                        hint["medias"].append(
-                            {
-                                "file": file,
-                            }
-                        )
-
-        # Extract media for choices
-        for choice in request_data["choices"]:
-            choice_medias = choice.pop("medias", [])
-            if choice["has_media"]:
-                choice["medias"] = []
-                for key in choice_medias:
-                    file = request.FILES.get(key)
-                    if file:
-                        choice["medias"].append(
-                            {
-                                "file": file,
-                            }
-                        )
-
-        return request_data
+    http_method_names = ["get", "post", "patch", "delete"]
 
     def get_serializer(self, *args, **kwargs):
-        if self.action in ["create", "update"]:
+        if self.action in ["create", "partial_update"]:
             return QuestionEditSerializer(*args, **kwargs)
         return super().get_serializer(*args, **kwargs)
 
     def create(self, request, *args, **kwargs):
-
-        # request_data = self.parse_media(request)
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         question = serializer.save()
         serializer = QuestionDetailSerializer(question)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        question = serializer.save()
+        serializer = QuestionDetailSerializer(question)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     @action(detail=False, methods=["delete"], url_path="delete-all")
     def delete_all(self, request):
