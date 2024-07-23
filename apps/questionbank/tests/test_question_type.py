43eff1aaf5cfd3102ff27c7900f3604d435f55f5
SheryarBaloch67 <sheryarbaloch67@gmail.com>
2024-07-23 13:17:22 +0500

    question type and retry hint test added

--- apps/questionbank/tests/test_question_type.py
+++ apps/questionbank/tests/test_question_type.py
@@ -51,7 +51,9 @@ class QuestionTypeTest(QuestionTypeUnitTest):
         json_data = self.do_get_question_type_list()
         self.assertGreater(len(json_data), 0)
         for test_dict in json_data:
-            for one_value_from_list_of_fields_of_question_type_model in (self.list_of_fields_of_question_type_model):
+            for (
+                one_value_from_list_of_fields_of_question_type_model
+            ) in self.list_of_fields_of_question_type_model:
                 self.assertIn(
                     one_value_from_list_of_fields_of_question_type_model,
                     test_dict,
@@ -65,9 +67,11 @@ class QuestionTypeTest(QuestionTypeUnitTest):
         self.assertEqual(
             json_data["id"],
             test_question_type_id,
-            f"The Field ID ({json_data['id']} is not equal to id ({test_question_type_id}) )",
+            f"The field id ({json_data['id']} is not equal to id ({test_question_type_id}) )",
         )
-        for one_value_from_list_of_fields_of_question_type_model in (self.list_of_fields_of_question_type_model):
+        for (
+            one_value_from_list_of_fields_of_question_type_model
+        ) in self.list_of_fields_of_question_type_model:
             self.assertIn(
                 one_value_from_list_of_fields_of_question_type_model,
                 json_data,
