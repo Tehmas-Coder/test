f7745ffdad956ff40d06d2cda00a452b7424d059
xeryar <sheryarbaloch67@gmail.com>
2024-07-24 13:35:10 +0500

    minor changes

--- apps/lookups/serializers/media_serializers.py
+++ apps/lookups/serializers/media_serializers.py
@@ -53,3 +53,63 @@ class MediaSerializer(BaseModelSerializer):
             return MEDIA_TYPES["video"]
         else:
             return MEDIA_TYPES["document"]
+
+
+class MediaBulkCreateSerializer(serializers.Serializer):
+    files = serializers.ListField(child=serializers.FileField())
+
+    def validate(self, data):
+        # Initialize a list to hold individual media validation errors
+        media_serializer_errors = []
+
+        # Iterate over each file and validate it using MediaSerializer
+        for file in data.get("files", []):
+            media_data = {"file": file}
+            media_serializer = MediaSerializer(data=media_data)
+
+            if media_serializer.is_valid():
+                # Validate successful, move to the next file
+                continue
+            else:
+                # Collect errors
+                media_serializer_errors.append(media_serializer.errors)
+
+        if media_serializer_errors:
+            raise serializers.ValidationError({"file_errors": media_serializer_errors})
+
+        return data
+
+    def create(self, validated_data):
+        files = validated_data.get("files", [])
+        media_instances = []
+
+        for file in files:
+            name = file.name
+            extension = file.name.split(".")[-1]
+            size = file.size
+            media_type = self.determine_media_type(file)
+
+            media_instance = Media(
+                name=name,
+                file=file,
+                extension=extension,
+                size=size,
+                type_id=media_type,
+            )
+            media_instances.append(media_instance)
+
+        # Bulk create media instances
+        Media.objects.bulk_create(media_instances)
+
+        return media_instances
+
+    def determine_media_type(self, file):
+        extension = file.name.split(".")[-1]
+        if extension in ["jpg", "jpeg", "png", "gif"]:
+            return MEDIA_TYPES["image"]
+        elif extension in ["mp3", "wav", "ogg"]:
+            return MEDIA_TYPES["audio"]
+        elif extension in ["mp4", "avi", "mov"]:
+            return MEDIA_TYPES["video"]
+        else:
+            return MEDIA_TYPES["document"]
