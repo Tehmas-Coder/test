16b660e1647b610382503d30e16d246b9243514d
xeryar <sheryarbaloch67@gmail.com>
2024-07-24 10:36:47 +0500

    seeds updated

--- scripts/win/seed.bat
+++ scripts/win/seed.bat
@@ -12,3 +12,6 @@ python manage.py loaddata education_level_seed.json
 python manage.py loaddata difficulty_level_seed.json
 python manage.py loaddata subject_seed.json
 python manage.py loaddata subject_education_level_seed.json
+python manage.py loaddata question_seed.json
+python manage.py loaddata question_choice_seed.json
+python manage.py loaddata question_retry_hint_seed.json
