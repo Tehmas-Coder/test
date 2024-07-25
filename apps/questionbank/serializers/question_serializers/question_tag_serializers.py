57968cc4dc47da992a6c7ebabcdd15da856b3614
xeryar <sheryarbaloch67@gmail.com>
2024-07-25 12:48:22 +0500

    Added Bulk Create Question Tags

--- apps/questionbank/serializers/question_serializers/question_tag_serializers.py
+++ apps/questionbank/serializers/question_serializers/question_tag_serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from core.serializers import BaseModelSerializer, get_base_model_fields
 from apps.questionbank.models import QuestionTag
 
@@ -5,4 +6,38 @@ from apps.questionbank.models import QuestionTag
 class QuestionTagSerializer(BaseModelSerializer):
     class Meta:
         model = QuestionTag
-        fields = ["id", "question", "tag"] + get_base_model_fields()
+        fields = [
+            "id",
+            "question",
+            "tag",
+        ] + get_base_model_fields()
+
+
+class QuestionTagBulkCreateSerializer(serializers.Serializer):
+    question = serializers.IntegerField()
+    tags = serializers.ListField(child=serializers.IntegerField())
+
+    def validate(self, data):
+        question_tag_serializer_errors = []
+        self.question_tags_instances_data = []
+
+        for tag in data.get("tags", []):
+            question_tag_data = {"question": data.get("question"), "tag": tag}
+            question_tag_serializer = QuestionTagSerializer(data=question_tag_data)
+            if not question_tag_serializer.is_valid():
+                question_tag_serializer_errors.append(question_tag_serializer.errors)
+            else:
+                self.question_tags_instances_data.append(question_tag_serializer.validated_data)
+
+        if question_tag_serializer_errors:
+            raise serializers.ValidationError({"question_tag_errors": question_tag_serializer_errors})
+
+        return data
+
+    def create(self, validated_data):
+        # * Bulk Create Question Tags
+        question_tags_instances = [QuestionTag(**data) for data in self.question_tags_instances_data]
+        QuestionTag.objects.bulk_create(question_tags_instances)
+        created_question_tags_instances = QuestionTag.objects.all().order_by("-created_at")[: len(question_tags_instances)]
+
+        return created_question_tags_instances
--- apps/questionbank/views/question_views.py
+++ apps/questionbank/views/question_views.py
@@ -57,6 +57,7 @@ from apps.questionbank.serializers.question_serializers.question_serializers imp
     QuestionEditSerializer,
 )
 from apps.questionbank.serializers.question_serializers.question_tag_serializers import (
+    QuestionTagBulkCreateSerializer,
     QuestionTagSerializer,
 )
 from apps.questionbank.serializers.question_serializers.question_type_serializers import (
@@ -242,6 +243,15 @@ class QuestionTagViewSet(viewsets.ModelViewSet):
     queryset = QuestionTag.objects.all()
     serializer_class = QuestionTagSerializer
     http_method_names = ["post", "delete"]
+
+    @action(detail=False, methods=["post"], url_path="bulk-create")
+    def bulk_create_question_tags(self, request):
+        request_data = request.data
+        serializer = QuestionTagBulkCreateSerializer(data=request_data)
+        serializer.is_valid(raise_exception=True)
+        question_tags = serializer.save()
+        serializer = QuestionTagSerializer(question_tags, many=True)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 # ---------------------------------- CHOICES --------------------------------- #
