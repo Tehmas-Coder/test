891fea06e3dfe76840fb562f191af92240c90969
xeryar <sheryarbaloch67@gmail.com>
2024-08-27 17:20:30 +0500

    User Test Revamped

--- apps/user/tests/test_user.py
+++ apps/user/tests/test_user.py
@@ -3,8 +3,12 @@ import json
 
 from rest_framework import status
 
+from apps.exam_public.models.exam_public_models import Candidate
+from apps.organization.models.organization_models import OrganizationUser
+from apps.user.serializers.user_serializers import UserDetailSerializer
 from core.test_setup import TestSetUp
 from utils.rna_utils import (
+    color_print,
     debug_print,
     print_test_failed,
     print_test_header,
@@ -13,7 +17,7 @@ from utils.rna_utils import (
 
 
 class UserUnitTest(TestSetUp):
-    fixtures = ["country_test_seed", "role_seed", "test_user_seed"]
+    fixtures = ["country_test_seed", "role_seed", "test_user_seed", "user_role_seed", "organization_seed", "organization_user_seed"]
 
     # ?###################################################
     # ?                  UNIT - TESTS
@@ -64,18 +68,27 @@ class UserTest(UserUnitTest):
         "last_name": "sheryar",
         "password": "123456789",
         "date_of_birth": "1995-07-27",
+        "country": 1,
         "phone": "+9323346489529",
         "role": 4,
+        "description": "This is a test user",
     }
     list_of_fields_of_user_model = [
         "id",
         "email",
         "first_name",
         "last_name",
-        "created_at",
-        "updated_at",
-        "otp",
+        "full_name",
+        "date_of_birth",
+        "profile_picture",
+        "roles",
+        "country",
+        "phone",
         "is_verified",
+        "is_superuser",
+        "date_joined",
+        "last_login",
+        "description",
     ]
 
     # ?###################################################
@@ -83,18 +96,82 @@ class UserTest(UserUnitTest):
     # ?###################################################
     def test_cases_user(self):
         self.successfull_creation_of_a_record_test()
-        self.failed_creation_of_a_duplicate_record_test()
-        list_of_records = self.successsfull_fetching_of_list_of_records_test()
-        test_record_id = self.successsfull_fetching_of_one_record_test(list_of_records)
-        self.successfull_updation_of_record_test(test_record_id)
+        # self.failed_creation_of_a_duplicate_record_test()
+        # list_of_records = self.successsfull_fetching_of_list_of_records_test()
+        # test_record_id = self.successsfull_fetching_of_one_record_test(list_of_records)
+        # self.successfull_updation_of_record_test(test_record_id)
 
     # ?###################################################
     # ?              TESTS - FUNCTIONS
     # ?###################################################
 
     def successfull_creation_of_a_record_test(self):
+        # ------------------------ User Creation By SuperUser ------------------------ #
         response = self.do_create_user(json.dumps(self.reuseable_request_body))
+        color_print("## => Testing User Creation by SuperUser")
+        json_data = response.data["data"]
+        for one_field in self.list_of_fields_of_user_model:
+            self.assertIn(one_field, json_data)
+        for key in self.reuseable_request_body:
+            if key == "password":
+                continue
+            if key == "role":
+                self.assertEqual(json_data["roles"][0]["id"], self.reuseable_request_body[key])
+                continue
+            self.assertEqual(json_data[key], self.reuseable_request_body[key])
         validate_success_201_test_response(self, response)
+        if json_data["roles"][0]["name"].lower() == "candidate":
+            candidate_instance = Candidate.objects.filter(user_id=json_data["id"]).first()
+            if not candidate_instance:
+                color_print("Failed: User created but Candidate not created", "red")
+
+        # -------------------- Candidate User Creation By Organization User -------------------- #
+        self.custom_login(email="user3@example.com", password=12345678)
+        request_body_for_organization_candidate = copy.deepcopy(self.reuseable_request_body)
+        request_body_for_organization_candidate["email"] = "sheryarbaloch57@gmail.com"
+        response = self.do_create_user(json.dumps(request_body_for_organization_candidate))
+        color_print("## => Testing Candidate User Creation by OrganizationUser")
+        json_data = response.data["data"]
+        for one_field in self.list_of_fields_of_user_model:
+            self.assertIn(one_field, json_data)
+        for key in request_body_for_organization_candidate:
+            if key == "password":
+                continue
+            if key == "role":
+                self.assertEqual(json_data["roles"][0]["id"], request_body_for_organization_candidate[key])
+                continue
+            self.assertEqual(json_data[key], request_body_for_organization_candidate[key])
+        validate_success_201_test_response(self, response)
+        if json_data["roles"][0]["name"].lower() == "candidate":
+            user_organization = OrganizationUser.objects.filter(user_id=self.user.id).values("organization").first()
+            candidate_instance = Candidate.objects.filter(user_id=json_data["id"], organization_id=user_organization["organization"]).first()
+            if not candidate_instance:
+                color_print("Failed: User created but Candidate not created", "red")
+
+        # -------------------- Organization Worker Creation By Organization User -------------------- #
+        request_body_for_organization_user = copy.deepcopy(self.reuseable_request_body)
+        request_body_for_organization_user["email"] = "sheryarbaloch77@gmail.com"
+        request_body_for_organization_user["role"] = 2
+        response = self.do_create_user(json.dumps(request_body_for_organization_user))
+        color_print("## => Testing Organization Worker Creation by OrganizationUser")
+        json_data = response.data["data"]
+        for one_field in self.list_of_fields_of_user_model:
+            self.assertIn(one_field, json_data)
+        for key in request_body_for_organization_user:
+            if key == "password":
+                continue
+            if key == "role":
+                self.assertEqual(json_data["roles"][0]["id"], request_body_for_organization_user[key])
+                continue
+            self.assertEqual(json_data[key], request_body_for_organization_user[key])
+        validate_success_201_test_response(self, response)
+        if json_data["roles"][0]["name"].lower() != "candidate":
+            user_organization = OrganizationUser.objects.filter(user_id=self.user.id).values("organization").first()
+            orgainzation_user_instance = OrganizationUser.objects.filter(
+                user_id=json_data["id"], organization_id=user_organization["organization"]
+            ).first()
+            if not orgainzation_user_instance:
+                color_print("Failed: User created but OrganizationUser not created", "red")
 
     def failed_creation_of_a_duplicate_record_test(self):
         response = self.do_create_user(self.reuseable_request_body)
