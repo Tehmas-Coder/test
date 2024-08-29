2a73823ec2d974fafdbe655f3197428a0bbe5f04
xeryar <sheryarbaloch67@gmail.com>
2024-08-29 16:23:02 +0500

    question retrieve and create updated and added the functionality of organization packages limit in Question only

--- apps/questionbank/views/question_views.py
+++ apps/questionbank/views/question_views.py
@@ -1,14 +1,17 @@
 import json
 
 from django.db import transaction
-from django.db.models import Q
+from django.db.models import F, Q
 from django.db.models.lookups import Exact
+from django.forms import model_to_dict
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.response import Response
 
 from apps.organization.models.organization_models import (
+    Organization,
+    OrganizationPackage,
     OrganizationQuestion,
     OrganizationUser,
 )
@@ -204,20 +207,33 @@ class QuestionViewSet(viewsets.ModelViewSet):
             request_data = self.parse_media(request)
         else:
             request_data = request.data
+
         # * Setting Question to public if the user is a superuser
         if request.user.is_superuser:
             request_data["is_public"] = 1
+
         serializer = self.get_serializer(data=request_data)
         serializer.is_valid(raise_exception=True)
         question = serializer.save()
+
         # * Assigning Question to Organization if the requested user is not superuser
         if not request.user.is_superuser:
             organization_id = OrganizationUser.objects.filter(user_id=request.user.id).values_list("organization", flat=True).first()
             if not organization_id:
                 transaction.set_rollback(True)
                 return make_error_response(message=f"Failed: User doesn't belong to any organization")
-            question_id = question.id
-            OrganizationQuestion.objects.create(organization_id=organization_id, question_id=question_id)
+            organization = Organization.objects.get(id=organization_id)
+            # * Checking the usage of questions of Users package
+            organization_package = (
+                OrganizationPackage.objects.filter(organization=organization).annotate(total_questions=F("package__questions")).last()
+            )
+            if not (organization_package.questions <= organization_package.total_questions):
+                transaction.set_rollback(True)
+                return make_error_response(message=f"Failed: Your limit to create questions is reached")
+            organization_package.questions = organization_package.questions + 1
+            organization_package.save()
+            organization.questions.add(question.id)
+
         serializer = QuestionDetailSerializer(question)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
@@ -227,6 +243,16 @@ class QuestionViewSet(viewsets.ModelViewSet):
             organization_question_ids = list(OrganizationQuestion.objects.filter(organization_id=organization_id).values_list("question", flat=True))
             self.queryset = self.queryset.filter(Q(id__in=organization_question_ids) | Q(is_public=True))
         return super().list(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        res = super().retrieve(request, *args, **kwargs)
+        if not request.user.is_superuser:
+            question_id = res.data["id"]
+            organization_id = OrganizationUser.objects.filter(user_id=request.user.id).values_list("organization", flat=True).first()
+            organization_question = OrganizationQuestion.objects.filter(organization_id=organization_id, question_id=question_id)
+            if not ((res.data["is_public"]) or len(organization_question)):
+                return make_error_response(message=f"Failed: This Question doesn't belong to your organization")
+        return res
 
     def partial_update(self, request, *args, **kwargs):
         request_data = request.data
