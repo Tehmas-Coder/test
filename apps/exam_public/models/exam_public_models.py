4f76aaa49cda99da6e0e3f3ee05ff02e2e7feeab
Rana Naeem Afzal <44816058+r-naeem-afzal@users.noreply.github.com>
2024-09-10 13:16:23 +0500

    minor refactor and type fix

--- apps/exam_public/models/exam_public_models.py
+++ apps/exam_public/models/exam_public_models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-from apps.exam_public.models.exam_public_backlog_models import ExamBacklogQuestion
 from apps.organization.models.organization_models import Organization
 from core.models import BaseModel
 
@@ -12,12 +11,9 @@ class Candidate(BaseModel):
     organization = models.ForeignKey(to=Organization, on_delete=models.CASCADE, null=True, blank=True, related_name="organization_candidates")
 
     def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        related_candidate_exams = CandidateExam.objects.filter(candidate_email=self.user.email)
-
-        for exam in related_candidate_exams:
-            exam.candidate = self
-            exam.save()
+        saved = super().save(*args, **kwargs)
+        CandidateExam.objects.filter(candidate_email=self.user.email).update(candidate=self)
+        return saved
 
     class Meta:
         app_label = "exam_public"
