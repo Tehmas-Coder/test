957f2ba5532f080b534ea5c209397249c58547fe
xeryar <sheryarbaloch67@gmail.com>
2024-10-08 18:21:18 +0500

    Refactor candidate exam scoresheet endpoint URL

--- apps/exam_scoring/urls.py
+++ apps/exam_scoring/urls.py
@@ -8,7 +8,7 @@ router = routers.DefaultRouter()
 
 urlpatterns = [
     path("mark-candidate-exam/", CandidateExamScoringViewset.as_view({"post": "candidate_exam_marking"})),
-    path("score-candidate-exam/", CandidateExamScoringViewset.as_view({"post": "candidate_exam_scoring"})),
+    path("candidate-exam-scoresheet/<int:id>/", CandidateExamScoringViewset.as_view({"get": "candidate_exam_scoresheet"})),
 ]
 
 urlpatterns += router.urls
