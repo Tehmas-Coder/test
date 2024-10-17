74b5dfd1689223a28b245d3cf086c9acff7d50ae
xeryar <sheryarbaloch67@gmail.com>
2024-10-17 16:18:06 +0500

    Refactor CountryViewset queryset to use get_detail_queryset method

--- apps/lookups/views.py
+++ apps/lookups/views.py
@@ -45,7 +45,7 @@ class RegionViewset(viewsets.ModelViewSet):
 class CountryViewset(viewsets.ModelViewSet):
     http_method_names = ["get"]
     serializer_class = CountryDetailSerializer
-    queryset = Country.objects.all().prefetch_related("timezones", "currencies", "languages", "states", "states__cities").order_by("name")
+    queryset = Country.get_detail_queryset()
     pagination_class = None
     permission_classes = []
 
