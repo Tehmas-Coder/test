d5a487abec132d58cac09081f465ee86fdd4000e
xeryar <sheryarbaloch67@gmail.com>
2024-10-24 14:57:06 +0500

    fixed ping view urlss

--- ping/ping_views.py
+++ ping/ping_views.py
@@ -1,6 +1,8 @@
 from typing import Any
+
+from rest_framework import status, views
 from rest_framework.response import Response
-from rest_framework import views, status
+
 from utils.rna_utils import print_test_header
 
 
@@ -8,6 +10,4 @@ class PingAPI(views.APIView):
 
     def get(self, request):
 
-        return Response(
-            data={"Status": "success", "Message": "pong"}, status=status.HTTP_200_OK
-        )
+        return Response(data={"Status": "success", "Message": "pong"}, status=status.HTTP_200_OK)
--- ping/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-
-from .views import ping_views
-
-urlpatterns = [path("/ping/", ping_views.PingAPI.as_view())]
