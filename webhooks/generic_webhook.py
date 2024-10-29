e4607f74bc043261e9e59936975d075d35897e9a
xeryar <sheryarbaloch67@gmail.com>
2024-10-29 14:29:47 +0500

    Refactor GenericWebhook class to send requests to student apply

---
+++ webhooks/generic_webhook.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+import requests
+from cryptography.fernet import Fernet
+
+
+@dataclass
+class GenericWebhook:
+    data_dict: dict
+
+    def send_request(self, encryption_key: str, token: str) -> bool:
+        webhook_url = self.__decrypt_token(encryption_key, token)
+        response = requests.post(webhook_url, json=self.data_dict)
+        return True if response.status_code == 200 else False
+
+    def __decrypt_token(self, encryption_key, token) -> str:
+        cipher = Fernet(encryption_key)
+        decrypted_message = cipher.decrypt(token).decode()
+        return decrypted_message
