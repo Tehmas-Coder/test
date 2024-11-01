7a4187639edfb5bcbf9510bf93a4e29fe8066b3b
xeryar <sheryarbaloch67@gmail.com>
2024-11-01 16:10:07 +0500

    Update permission assignment logic in RoleNinja to respect user context

--- apps/user/helpers/role_ninja.py
+++ apps/user/helpers/role_ninja.py
@@ -1,4 +1,5 @@
 from apps.user.models import Permission, Role
+from core.middlewares.current_user_middleware import get_current_user
 
 
 class RoleNinja:
@@ -10,7 +11,10 @@ class RoleNinja:
         """
         Add all permissions to a role with is_active set to False
         """
-        permission_ids_list = list(Permission.objects.all().values_list("id", flat=True))
+        if get_current_user().is_superuser:  # type: ignore
+            permission_ids_list = list(Permission.objects.all().values_list("id", flat=True))
+        else:
+            permission_ids_list = list(Permission.objects.exclude(context_value__in=["studentapply", "candidates"]).values_list("id", flat=True))
         new_role_instance = Role.objects.prefetch_related("permissions").get(pk=user_id)
         if len(permission_ids_list):
             new_role_instance.permissions.set(permission_ids_list)
