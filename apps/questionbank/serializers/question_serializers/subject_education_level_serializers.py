87acdbd38b88ed68289f0f2939f5134c10e2158f
xeryar <sheryarbaloch67@gmail.com>
2024-11-08 12:05:05 +0500

    Refactor serializers to replace SubjectListSerializer with SubjectSerializer; update education level handling and organization assignment logic

--- apps/questionbank/serializers/question_serializers/subject_education_level_serializers.py
+++ apps/questionbank/serializers/question_serializers/subject_education_level_serializers.py
@@ -3,13 +3,13 @@ from apps.questionbank.serializers.question_serializers.education_level_serializ
     EducationLevelSerializer,
 )
 from apps.questionbank.serializers.question_serializers.subject_serializers import (
-    SubjectListSerializer,
+    SubjectSerializer,
 )
 from core.serializers import BaseModelSerializer, get_base_model_fields
 
 
 class SubjectEducationLevelDetailSerializer(BaseModelSerializer):
-    subject = SubjectListSerializer()
+    subject = SubjectSerializer()
     education_level = EducationLevelSerializer()
 
     class Meta:
