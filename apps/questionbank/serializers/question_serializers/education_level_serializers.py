1a6660602052947f99829dab8d638d71df29434d
xeryar <sheryarbaloch67@gmail.com>
2024-11-08 12:04:39 +0500

    Add organization handling in EducationLevelSerializer; restrict organization assignment for non-superusers

--- apps/questionbank/serializers/question_serializers/education_level_serializers.py
+++ apps/questionbank/serializers/question_serializers/education_level_serializers.py
@@ -1,4 +1,6 @@
 from apps.questionbank.models import EducationLevel
+from apps.user.utils.utils import get_current_user_organization
+from core.middlewares.current_user_middleware import get_current_user
 from core.serializers import BaseModelSerializer, get_base_model_fields
 
 
@@ -10,4 +12,10 @@ class EducationLevelSerializer(BaseModelSerializer):
             "name",
             "code",
             "abbreviation",
+            "organization",
         ] + get_base_model_fields()
+
+    def create(self, validated_data):
+        if not get_current_user().is_superuser:  # type: ignore
+            validated_data["organization_id"] = get_current_user_organization()
+        return super().create(validated_data)
