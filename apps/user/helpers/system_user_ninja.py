d802eab94e28fecee8bff93d2c7170183e636265
xeryar <sheryarbaloch67@gmail.com>
2024-11-12 15:59:11 +0500

    Refactor CreateSystemUserAPI to utilize SystemUserNinja for user creation logic and streamline role handling

---
+++ apps/user/helpers/system_user_ninja.py
@@ -0,0 +1,91 @@
+from rest_framework import status
+from rest_framework.response import Response
+
+from apps.organization.models.organization_models import OrganizationUser
+from apps.user.models import BaseUser, Role, UserRole
+from apps.user.utils.utils import get_current_user_organization
+from core.middlewares.response_middleware import ResponseMiddleware
+
+
+class SystemUserNinja:
+    def __init__(self, request_data):
+        self.data_dict: dict = request_data
+        self.roles = None
+        self.role_slug_id_hashmap: dict = {}
+        self.existing_users_email_and_instance_hashmap: dict = {}
+        self.existing_organization_users_email_and_instance_hashmap: dict = {}
+        self.unentertained_emails: list = []
+
+    def create_system_user(self):
+        self.__collect_roles()
+        self.__fetch_users_and_organization_users()
+        self.__process_system_users()
+        return self.unentertained_emails
+
+    def __collect_roles(self):
+        role_slugs = [f"{get_current_user_organization()}-{one_dict['Slug']}" for one_dict in self.data_dict]
+        slugs_to_role_name_hashmap = {f"{get_current_user_organization()}-{one_dict['Slug']}": one_dict["RoleName"] for one_dict in self.data_dict}
+
+        self.roles = Role.objects.filter(slug__in=role_slugs, is_system_role=True)
+        found_role_slugs = set(self.roles.values_list("slug", flat=True))
+        missing_slugs = set(role_slugs) - found_role_slugs
+
+        if missing_slugs:
+            missing_role_names = [slugs_to_role_name_hashmap[slug] for slug in missing_slugs]
+            ResponseMiddleware.return_now(
+                Response(
+                    {"status": "failed", "message": f"These roles are not created in QB yet: {', '.join(missing_role_names)}."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            )
+        self.role_slug_id_hashmap = {role.slug: role.id for role in self.roles}  # type: ignore
+
+    def __fetch_users_and_organization_users(self):
+        emails = [one_user["Email"] for one_user in self.data_dict]
+
+        existing_users = BaseUser.objects.filter(email__in=emails)
+        self.existing_users_email_and_instance_hashmap = {user.email: user for user in existing_users}
+
+        organization_users = OrganizationUser.objects.filter(user__email__in=emails).select_related("organization", "user")
+        self.existing_organization_users_email_and_instance_hashmap = {org_user.user.email: org_user for org_user in organization_users}
+
+    def __process_system_users(self):
+        for one_user in self.data_dict:
+            role_slug = f"{get_current_user_organization()}-{one_user['Slug']}"
+            role_id = self.role_slug_id_hashmap[role_slug]
+            email = one_user["Email"]
+            to_delete = one_user.get("ToDelete", False)
+
+            user_instance = self.existing_users_email_and_instance_hashmap.get(email)
+            create_organization_user = False
+
+            if email in self.existing_organization_users_email_and_instance_hashmap:
+                org_user = self.existing_organization_users_email_and_instance_hashmap[email]
+                if org_user.organization_id != get_current_user_organization():  # type: ignore
+                    self.unentertained_emails.append(email)
+                    continue
+            else:
+                create_organization_user = True
+
+            if not user_instance:
+                user_instance = self.__create_system_user(email, role_id, one_user)
+            else:
+                if to_delete:
+                    UserRole.objects.filter(user=user_instance, role_id=role_id).update(meta_status="deleted")
+                else:
+                    UserRole.objects.get_or_create(user=user_instance, role_id=role_id, defaults={"user": user_instance, "role_id": role_id})
+
+            if create_organization_user:
+                OrganizationUser.objects.create(user=user_instance, organization_id=get_current_user_organization())
+
+    def __create_system_user(self, email, role_id, one_user):
+        user_instance = BaseUser.objects.create(
+            email=email,
+            first_name=one_user["FirstName"],
+            last_name=one_user["LastName"],
+            phone=one_user["PhoneNumber"],
+            date_of_birth=one_user["DateOfBirth"],
+        )
+        UserRole.objects.create(user=user_instance, role_id=role_id)
+        self.existing_users_email_and_instance_hashmap[email] = user_instance
+        return user_instance
