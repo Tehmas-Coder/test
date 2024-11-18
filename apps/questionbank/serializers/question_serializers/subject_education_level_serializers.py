8f37774ebf621d40a37fe2d30b5f82c90adb5747
xeryar <sheryarbaloch67@gmail.com>
2024-11-18 18:38:30 +0500

    Refactor SubjectEducationLevel serializers to improve structure and add representation logic, and merge the 2 serializers into one

--- apps/questionbank/serializers/question_serializers/subject_education_level_serializers.py
+++ apps/questionbank/serializers/question_serializers/subject_education_level_serializers.py
@@ -8,10 +8,7 @@ from apps.questionbank.serializers.question_serializers.subject_serializers impo
 from core.serializers import BaseModelSerializer, get_base_model_fields
 
 
-class SubjectEducationLevelDetailSerializer(BaseModelSerializer):
-    subject = SubjectSerializer()
-    education_level = EducationLevelSerializer()
-
+class SubjectEducationLevelSerializer(BaseModelSerializer):
     class Meta:
         model = SubjectEducationLevel
         fields = [
@@ -19,16 +16,6 @@ class SubjectEducationLevelDetailSerializer(BaseModelSerializer):
             "subject",
             "education_level",
             "organization",
-        ] + get_base_model_fields()
-
-
-class SubjectEducationLevelEditSerializer(BaseModelSerializer):
-    class Meta:
-        model = SubjectEducationLevel
-        fields = [
-            "id",
-            "subject",
-            "education_level",
         ] + get_base_model_fields()
 
         read_only_fields = ["id"]
@@ -39,5 +26,11 @@ class SubjectEducationLevelEditSerializer(BaseModelSerializer):
             education_level=validated_data["education_level"],
             defaults=validated_data,
         )
-
         return instance
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if not self.context.get("mutator"):
+            representation["subject"] = SubjectSerializer(instance.subject).data
+            representation["education_level"] = EducationLevelSerializer(instance.education_level).data
+        return representation
