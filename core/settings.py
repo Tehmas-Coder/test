fdbe55f59c43e425155a627364a101cba86bb141
xeryar <sheryarbaloch67@gmail.com>
2024-12-23 15:21:47 +0500

    Add support for mock file uploads and configure media storage settings

--- core/settings.py
+++ core/settings.py
@@ -44,7 +44,15 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 128000000
 ROOT_URLCONF = "core.urls"
 WSGI_APPLICATION = "core.wsgi.application"
 EMAIL_BACKEND = "django_ses.SESBackend"
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+MOCK_FILE_UPLOAD = config("MOCK_FILE_UPLOAD", default=False, cast=bool)
+
+if MOCK_FILE_UPLOAD:
+    DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+    MEDIA_ROOT = BASE_DIR / "media"
+    MEDIA_URL = "/media/"
+else:
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 #! SECURITY WARNING: keep the secret key used in production secret!
 try:
