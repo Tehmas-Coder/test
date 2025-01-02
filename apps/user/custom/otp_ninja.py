7bdfd03088e3c9c264497835bf114054bff2b840
xeryar <sheryarbaloch67@gmail.com>
2025-01-02 18:00:11 +0500

    Add OTP expiration handling and warning responses in OTPNinja class

--- apps/user/custom/otp_ninja.py
+++ apps/user/custom/otp_ninja.py
@@ -1,6 +1,6 @@
 from apps.user.models.user_models import BaseUser
 from middlewares.response_middleware import ResponseMiddleware
-from utils.rna_utils import make_error_response
+from utils.rna_utils import make_error_response, make_warning_response
 
 
 class OTPNinja:
@@ -10,11 +10,15 @@ class OTPNinja:
 
     def verify_otp(self, otp):
         self.__validate_user()
+        if self.user.is_otp_expired:  # type: ignore
+            ResponseMiddleware.return_now(make_warning_response(message="OTP expired! Please request for another OTP"))
         if not self.user.verify_otp(otp):  # type: ignore
             ResponseMiddleware.return_now(make_error_response(message="Invalid OTP!"))
 
     def resend_otp(self):
         self.__validate_user()
+        if not self.user.is_otp_expired:  # type: ignore
+            ResponseMiddleware.return_now(make_warning_response(message="OTP already sent! Please check your email for the OTP"))
         if not self.user.send_otp():  # type: ignore
             ResponseMiddleware.return_now(make_error_response(message="Failed to send OTP, please try again"))
 
