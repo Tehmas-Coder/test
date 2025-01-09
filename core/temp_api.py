3d30ee11a3287e6d7508d78ce0e64cad50799b80
xeryar <sheryarbaloch67@gmail.com>
2025-01-09 11:13:50 +0500

    Uncomment localhost restriction in TempApi get method

--- core/temp_api.py
+++ core/temp_api.py
@@ -19,9 +19,8 @@ class TempApi(APIView):
 
     def get(self, request, *args, **kwargs):
         # if request is not from localhost, return 404
-        # TODO: Uncomment the following code block to restrict the API to localhost after testing on production is done
-        # if request.META["REMOTE_ADDR"] not in ["localhost", "127.0.0.1"]:
-        #     return Response(status=404)
+        if request.META["REMOTE_ADDR"] not in ["localhost", "127.0.0.1"]:
+            return Response(status=404)
         # for i in range(1, 3):
         #     send_email_task(
         #         subject=f"Test Email {i} from Django",
