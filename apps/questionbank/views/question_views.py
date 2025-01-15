347e036eeb17ab49e6250258b854ae2ffeb1bb16
xeryar <sheryarbaloch67@gmail.com>
2025-01-15 13:22:54 +0500

    Remove transaction rollback comment in question cloning response

--- apps/questionbank/views/question_views.py
+++ apps/questionbank/views/question_views.py
@@ -219,6 +219,8 @@ class QuestionViewSet(viewsets.ModelViewSet):
         cloned_question_instance = QuestionClone().clone_question(original_question)
         cloned_question = Question.get_detail_queryset(q_filter=Q(id=cloned_question_instance.id), all=True).first()  # type: ignore
         data = QuestionSerializer(cloned_question).data
+        # TODO: Remove this transaction rollback when the response is fixed
+        # transaction.set_rollback(True)
         return Response({"data": data}, status=status.HTTP_201_CREATED)
 
 
