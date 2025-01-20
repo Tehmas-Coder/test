c0a2b83083fe9a243e4fd8b4be4055e1c5bf846f
xeryar <sheryarbaloch67@gmail.com>
2025-01-20 15:51:03 +0500

    Refactor logging middleware to streamline log file creation and prepare for S3 upload

--- middlewares/logging_middleware.py
+++ middlewares/logging_middleware.py
@@ -4,11 +4,16 @@ import sys
 import time
 from datetime import datetime, timezone
 
+import boto3
+from decouple import config
+
 
 class LoggingMiddleware:
 
     def __init__(self, get_response):
         self.get_response = get_response
+        self.s3_client = boto3.client("s3")
+        self.bucket_name = config("AWS_STORAGE_BUCKET_NAME")
 
     def __call__(self, request):
         # Skip the logs in Test Environment
@@ -34,8 +39,9 @@ class LoggingMiddleware:
         if not os.path.exists(log_directory):
             os.makedirs(log_directory)
 
+        log_filename = f"{log_directory}/{datetime.now(timezone.utc).strftime('%H')}.log"
         logger = logging.getLogger("")
-        handler = logging.FileHandler(filename=f"{log_directory}/{datetime.now(timezone.utc).strftime('%H')}.log")
+        handler = logging.FileHandler(filename=log_filename)
         formatter = logging.Formatter("%(asctime)s %(message)s")
         formatter.converter = time.gmtime
         formatter._fmt = "%Y-%m-%d %H:%M:%S %Z %(message)s"
@@ -54,65 +60,9 @@ class LoggingMiddleware:
         logger.removeHandler(handler)
         handler.close()
 
+        # Upload log file to S3
+        # s3_log_directory = f"logs/{user}/{str(datetime.today().date())}"
+        # s3_log_filename = f"{s3_log_directory}/{datetime.now(timezone.utc).strftime('%H')}.log"
+        # self.s3_client.upload_file(log_filename, self.bucket_name, s3_log_filename)
+
         return response
-
-
-# import logging
-# import os
-# import time
-# from datetime import datetime, timezone
-
-# import boto3
-# from botocore.exceptions import NoCredentialsError
-# from decouple import config
-
-
-# class LoggingMiddleware:
-
-#     def __init__(self, get_response):
-#         self.get_response = get_response
-#         self.s3_client = boto3.client("s3")
-#         self.bucket_name = config("AWS_STORAGE_BUCKET_NAME")
-
-#     def __call__(self, request):
-#         start_time = time.time()
-
-#         response = self.get_response(request)
-
-#         end_time = time.time()
-#         duration = (end_time - start_time) * 1000
-#         user = request.user.pk if request.user.is_authenticated else "anonymous"
-#         log_directory = f"media/logs/{user}/{str(datetime.today().date())}"
-#         if not os.path.exists(log_directory):
-#             os.makedirs(log_directory)
-
-#         log_filename = f"{log_directory}/{datetime.now(timezone.utc).strftime('%H')}.log"
-#         logger = logging.getLogger("")
-#         handler = logging.FileHandler(filename=log_filename)
-#         formatter = logging.Formatter("%Y-%m-%d %H:%M:%S %Z %(message)s")
-#         formatter.converter = time.gmtime
-#         handler.setFormatter(formatter)
-#         logger.addHandler(handler)
-#         logger.setLevel(logging.INFO)
-#         log_data = {
-#             "Time Taken (ms)": duration.__ceil__(),
-#             "Request": {
-#                 "method": request.method,
-#                 "path": request.path,
-#                 "user": user,
-#                 "query_params": str(request.GET) if request.GET else None,
-#                 "data": request.body if request.method == "POST" else None,
-#             },
-#             "Response": {
-#                 "status_code": response.status_code,
-#                 # "data": response.content if response.status_code != 500 else None,
-#             },
-#         }
-#         logger.info(f"{log_data}")
-#         logger.removeHandler(handler)
-#         handler.close()
-
-#         # Upload log file to S3
-#         self.s3_client.upload_file(log_filename, self.bucket_name, log_filename)
-
-#         return response
