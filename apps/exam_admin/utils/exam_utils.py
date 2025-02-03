5d158166060907f7b8e3564d45aa8be340f37b5b
xeryar <sheryarbaloch67@gmail.com>
2025-02-03 14:55:44 +0500

    Refactor random exam creation logic into a RandomExamCreator class for improved structure and maintainability

--- apps/exam_admin/utils/exam_utils.py
+++ apps/exam_admin/utils/exam_utils.py
@@ -1,112 +1,100 @@
-from typing import Any, Dict, List, Union
+from dataclasses import dataclass
+from typing import Any
 
-from rest_framework.utils.serializer_helpers import ReturnList
+from django.db.models import Model, Prefetch, QuerySet
 
-from apps.exam_admin.models.exam_admin_models import Exam
-from apps.exam_admin.serializers.exam_serializers import ExamSerializer
-from apps.exam_admin.serializers.exam_subject_question_serializer import (
-    ExamSubjectQuestionSerializer,
+from apps.exam_admin.custom.exam_classes import (
+    ExamService,
+    ExamVisibilitySetter,
+    OrganizationPackageExamLimitValidator,
 )
-from apps.questionbank.models.question_models import Question, Subject
+from apps.exam_admin.models.exam_admin_models import ExamSubjectQuestion
+from apps.exam_admin.serializers.exam_serializers import ExamSerializer
+from apps.lookups.custom.lookups_classes import OrganizationResourceQuerysetMutator
+from apps.questionbank.models.question_models import Question, QuestionSubject
 from middlewares.response_middleware import ResponseMiddleware
-from utils.rna_utils import make_error_response, object_contains_all_values
+from utils.rna_utils import debug_print, make_error_response
 
 
-def create_exam_instance(exam_data):
-    exam = ExamSerializer(data=exam_data, context={"mutator": True})
-    exam.is_valid(raise_exception=True)
-    return exam.save()
+@dataclass
+class RandomExamCreator:
+    exam_data: Any
+    subject_education_levels: Any
+    difficulty_levels: Any
+    question_types: Any
+    question_count: Any
 
+    def __post_init__(self):
+        self.created_exam_instance = None
 
-def create_random_exam(
-    exam_data: Dict[str, Any],
-    subject_question_count: Dict[str, int],
-    subject_count: int,
-    education_level_id: int,
-) -> Union[ReturnList, Any]:
-    """
-    This function generates a random exam with a given number of questions from a given question bank.
-    """
+    def create_random_exam(self) -> Model:
+        try:
+            self.__request_values_validator()
+        except Exception as e:
+            ResponseMiddleware.return_now(make_error_response(message=str(e)))
+        self.__create_exam_instance()
+        self.__create_exam_subjects_questions()
+        return self.created_exam_instance  # type: ignore
 
-    def validate_inputs() -> None:
-        if not subject_question_count:
-            ResponseMiddleware.return_now(make_error_response(message="Question count is required!"))
-        if not subject_count:
-            ResponseMiddleware.return_now(make_error_response(message="Subject count is required!"))
-        if not education_level_id:
-            ResponseMiddleware.return_now(make_error_response(message="Education level is required!"))
-        return None
+    # ---------------------------------------------------------------------------- #
+    #                                PRIVATE METHODS                               #
+    # ---------------------------------------------------------------------------- #
 
-    def select_subjects() -> None:
-        if not exam_data.get("subjects"):
-            # * If subjects are not provided, select random subjects which have at least one question based on subject count
-            subjects = Subject.select_random_subjects(subject_question_count, subject_count, education_level_id)
-            if not subjects:
-                ResponseMiddleware.return_now(make_error_response(message="Not enough subjects found for the given criteria!"))
-            exam_data["subjects"] = subjects
-        return None
+    def __request_values_validator(self):
+        if not self.exam_data:
+            raise ValueError("Exam data is required")
+        if not self.subject_education_levels:
+            raise ValueError("Subject education levels are required")
+        if not self.question_count:
+            raise ValueError("Question count is required")
 
-    def get_subject_id(original_exam_data: Dict[str, Any], subject_id: str) -> Union[None, str]:
-        if not original_exam_data.get("subjects"):
-            # * Break if array index is out of range
-            if int(subject_id) >= len(exam_data["subjects"]):
-                return None
-            # * We select the subject_id from the list of random subjects one by one
-            return exam_data["subjects"][int(subject_id)]
-        return subject_id
+    def __create_exam_instance(self):
+        self.exam_data["subjects"] = self.subject_education_levels
+        visibility_setter = ExamVisibilitySetter()
+        organization_validator = OrganizationPackageExamLimitValidator()
+        exam_service = ExamService(
+            exam_data=self.exam_data,
+            visibility_setter=visibility_setter,
+            organization_validator=organization_validator,
+            serializer_class=ExamSerializer,
+        )
+        self.created_exam_instance = exam_service.create_exam()
 
-    def fetch_random_questions() -> Dict[str, List[int]]:
-        random_subject_questions = {}
-        for subject_id, question_count in subject_question_count.items():
-            # * If subjects are not provided, then the subject_id is to be treated as the index of the random_subjects list
-            subject_id = get_subject_id(original_exam_data, subject_id)
-            if subject_id is None:
-                break
-            random_subject_questions[subject_id] = Question.select_random_questions(education_level_id, subject_id, question_count)
-        return random_subject_questions
+    def __create_exam_subjects_questions(self):
+        exam_marks = 0
+        questions = self.__get_questions()
+        exam_subjects = list(self.created_exam_instance.subjects.through.objects.filter(exam_id=self.created_exam_instance.pk))  # type: ignore
+        exam_subject_questions = []
+        question_sequence = 0
+        for question in questions:
+            question_sequence = question_sequence + 1
+            question_subject = list(question.subjects.all())[0]  # type:ignore
+            for exam_subject in exam_subjects:
+                if exam_subject.subject_education_level == question_subject.subject_education_level:
+                    exam_subject_questions.append(
+                        ExamSubjectQuestion(
+                            exam_subject_id=exam_subject.pk,
+                            question_id=question.pk,
+                            total_marks=question_subject.total_marks,
+                            sequence=question_sequence,
+                        )
+                    )
+                    exam_marks = exam_marks + question_subject.total_marks
+                    break
+        ExamSubjectQuestion.objects.bulk_create(exam_subject_questions)
+        self.created_exam_instance.total_marks = exam_marks  # type:ignore
+        self.created_exam_instance.save()  # type:ignore
 
-    def assign_questions_to_subjects(exam_instance: Any, random_subject_questions: Dict[str, List[int]]) -> None:
-        # * Assign Questions to Subjects
-        for subject_id, question_ids in random_subject_questions.items():
-            for sequence, question_id in enumerate(question_ids, start=1):
-                exam_question = ExamSubjectQuestionSerializer(
-                    data={
-                        "exam_subject": {
-                            "subject": subject_id,
-                            "exam": exam_instance.id,  # type: ignore
-                        },
-                        "question": question_id,
-                        "sequence": sequence,
-                    }
-                )
-                exam_question.is_valid(raise_exception=True)
-                exam_question.save()
-
-    def create_exam_instance(exam_data: Dict[str, Any]) -> Any:
-        # * Create Exam
-        exam = ExamSerializer(data=exam_data, context={"mutator": True})
-        exam.is_valid(raise_exception=True)
-        return exam.save()
-
-    error_response = validate_inputs()
-    if error_response:
-        return error_response
-
-    original_exam_data = exam_data.copy()
-
-    error_response = select_subjects()
-
-    random_subject_questions = fetch_random_questions()
-
-    # * Check if enough questions are found for the given criteria
-    all_subjects_have_questions = object_contains_all_values(random_subject_questions)
-    if not all_subjects_have_questions:
-        ResponseMiddleware.return_now(make_error_response(message="Not enough questions found for the given criteria!"))
-
-    exam_instance = create_exam_instance(exam_data=exam_data)
-
-    assign_questions_to_subjects(exam_instance, random_subject_questions)
-
-    exam_qs = Exam.get_detail_queryset(all=True).filter(id=exam_instance.id)  # type: ignore
-
-    return ExamSerializer(exam_qs.first(), context={"selector": True}).data
+    def __get_questions(self) -> QuerySet:
+        question_queryset = (
+            Question.objects.filter(
+                type__in=self.question_types,
+                subjects__difficulty_level__in=self.difficulty_levels,
+                subjects__subject_education_level__in=self.subject_education_levels,
+            )
+            .prefetch_related(Prefetch("subjects", queryset=QuestionSubject.objects.select_related("subject_education_level")))
+            .distinct()
+            .order_by("?")
+        )
+        questions = OrganizationResourceQuerysetMutator(queryset=question_queryset, is_public=True).get_queryset()[: self.question_count]
+        return questions
