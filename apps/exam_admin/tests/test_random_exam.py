54fe00081b533d2a0507795b1f9031cd1be222c4
xeryar <sheryarbaloch67@gmail.com>
2025-02-10 16:12:29 +0500

    Add tests for random exam creation and enhance request body structure

--- apps/exam_admin/tests/test_random_exam.py
+++ apps/exam_admin/tests/test_random_exam.py
@@ -1,11 +1,21 @@
+import json
+
 from rest_framework import status
 
 from core.test_setup import TestSetUp
-from utils.rna_utils import print_test_failed, print_test_passed
+from utils.rna_utils import (
+    debug_print,
+    print_test_failed,
+    print_test_header,
+    print_test_passed,
+)
 
 
 class RandomExamUnitTest(TestSetUp):
     fixtures = [
+        "tag_seed",
+        "difficulty_level_seed",
+        "measuring_unit_seed",
         "timezone_test_seed",
         "currency_test_seed",
         "language_test_seed",
@@ -13,6 +23,13 @@ class RandomExamUnitTest(TestSetUp):
         "subject_seed",
         "education_level_seed",
         "subject_education_level_seed",
+        "question_type_seed",
+        "question_seed",
+        "question_subject_seed",
+        "question_subject_country_seed",
+        "question_retry_hint_seed",
+        "question_choice_seed",
+        "question_tag_seed",
         "exam_seed",
         "user_seed",
         "role_seed",
@@ -26,25 +43,36 @@ class RandomExamUnitTest(TestSetUp):
     # ?                  UNIT - TESTS
     # ?###################################################
 
+    def create_random_exam(self, request_body):
+        print_test_header("create_random_exam")
+        url = "/api/exams/create-random/"
+        response = self.client.post(
+            url,
+            headers=self.headers,
+            data=request_body,
+            content_type="application/json",
+        )
+        validate_success_201_test_response(self, response)
+        return response.data  # type: ignore
+
 
 class RandomExamTest(RandomExamUnitTest):
     # * These are defined here so these can be accessed by all the functions
-    reuseable_request_body = {
-        "name": "Random Test Exam",
-        "code": "RET-1",
-        "abbreviation": "ret1",
-        "instructions": "No Instructions",
-        "education_level": 1,
-        "passing_percentage": 50,
-        "exam_status": "draft",
-        "is_global": 1,
-        "subject_education_levels": [9, 10, 11, 12],
+    request_body = {
+        "exam_data": {
+            "name": "exam 3",
+            "code": "E-3",
+            "abbreviation": "xyz",
+            "instructions": "asd",
+            "education_level": 1,
+            "passing_percentage": 50,
+            "is_global": 1,
+            "exam_status": "draft",
+        },
+        "subjects": [5, 6],
         "difficulty_levels": [1],
+        "question_types": [1, 2, 3],
         "question_count": 10,
-        "organization": 2,
-        "is_exam_preparatory": False,
-        "exam_duration": 120,
-        "exam_questions_visibility": "all-at-once",
     }
     list_of_fields_of_exam_model = [
         "id",
@@ -66,6 +94,13 @@ class RandomExamTest(RandomExamUnitTest):
     # ?###################################################
     # ?              TESTS - CASES
     # ?###################################################
+    def test_cases_random_exam(self):
+        self.successfull_creation_of_a_record_test(self.request_body)
+
+    def successfull_creation_of_a_record_test(self, request_body):
+        response_data = self.create_random_exam(json.dumps(request_body))
+        for field in self.list_of_fields_of_exam_model:
+            self.assertIn(field, response_data.keys(), f"field '{field}' not found in response data")
 
 
 # ?###################################################
