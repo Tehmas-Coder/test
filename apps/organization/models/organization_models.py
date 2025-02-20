1e0ae587cb77db70ba227772b1f2b33b538fd804
xeryar <sheryarbaloch67@gmail.com>
2025-02-20 13:17:09 +0500

    Enhance docstrings in organization models for clarity and documentation

--- apps/organization/models/organization_models.py
+++ apps/organization/models/organization_models.py
@@ -4,6 +4,14 @@ from core.models import BaseModel
 
 
 class OrganizationUser(BaseModel):
+    """
+    Represents a mapping between organizations and users.
+
+    - id: Autofield (PK)
+    - organization: Organization (FK)
+    - user: BaseUser (FK)
+    """
+
     organization = models.ForeignKey("lookups.Organization", on_delete=models.CASCADE, related_name="organization_users")
     user = models.ForeignKey("user.BaseUser", on_delete=models.CASCADE, related_name="user_organizations")
 
@@ -13,6 +21,19 @@ class OrganizationUser(BaseModel):
 
 
 class OrganizationPackage(BaseModel):
+    """
+    Represents a package assigned to an organization.
+
+    - id: Autofield (PK)
+    - organization: Organization (FK)
+    - package: Package (FK)
+    - users: PositiveIntegerField
+    - questions: PositiveIntegerField
+    - exams: PositiveIntegerField
+    - prep_exams: PositiveIntegerField
+    - exam_attempts: PositiveIntegerField
+    """
+
     organization = models.ForeignKey("lookups.Organization", on_delete=models.CASCADE, related_name="organization_packages")
     package = models.ForeignKey("lookups.Package", on_delete=models.CASCADE)
 
