979c36bca7cd68750f7b42062b064a6787451d68
xeryar <sheryarbaloch67@gmail.com>
2025-02-21 11:54:12 +0500

    Enhance docstrings for GenericWebhook class and send_request method to improve clarity and documentation

--- hooks/webhooks.py
+++ hooks/webhooks.py
@@ -8,9 +8,20 @@ from utils.rna_utils import decrypt_message, encrypt_message
 
 @dataclass
 class GenericWebhook:
+    """
+    Generic Webhook class to send data to any webhook
+    """
+
     data_dict: dict
 
     def send_request(self, encryption_key: str, token: str) -> bool:
+        """
+        Send a POST request to the webhook URL with the encrypted data
+
+        :param encryption_key: The encryption key to encrypt the data
+        :param token: The token to decrypt the webhook URL
+        :return: True if the request was successful, False otherwise
+        """
         webhook_url = decrypt_message(token, encryption_key)
         encrypted_data = encrypt_message(json.dumps(self.data_dict), encryption_key)
         response = requests.post(webhook_url, json=encrypted_data)
