db46dc32a0a9f8dc0fd5356f8c638b9019064e98
xeryar <sheryarbaloch67@gmail.com>
2025-02-21 11:53:02 +0500

    Enhance docstring for get_encryption_key function to improve clarity and documentation

--- helpers/helper_functions.py
+++ helpers/helper_functions.py
@@ -2,4 +2,9 @@ from core.settings import ENCRYPTION_KEY
 
 
 def get_encryption_key():
+    """
+    Get the encryption key from the settings file
+
+    :return: The encryption key (str)
+    """
     return ENCRYPTION_KEY
