36ff9d70b6a4f185454f4441ecd77d1f3f280124
xeryar <sheryarbaloch67@gmail.com>
2025-02-28 11:06:08 +0500

    Remove TODO comment regarding atomic API and exam expiration logic in CandidateExamNinja class

--- apps/exam_public/custom/candidate_exam_classes.py
+++ apps/exam_public/custom/candidate_exam_classes.py
@@ -492,7 +492,6 @@ class CandidateExamNinja:
         if not candidate_exam_instance:
             ResponseMiddleware.return_now(make_error_response(message="Candidate Exam not found"))
 
-        # TODO: Make this API atomic after the exam expiration logic is fixed and does not rollback the transaction on return_now
         if candidate_exam_instance.is_expired():  # type:ignore
             ResponseMiddleware.return_now(make_error_response(message="Exam has expired"))
 
