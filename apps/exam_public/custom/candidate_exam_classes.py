a82dd34c2c5613fcaaa309118ca2eec54b0e4f75
xeryar <sheryarbaloch67@gmail.com>
2025-02-28 11:04:02 +0500

    Refactor get_candidate_exam method to remove deprecated token logic and streamline candidate exam retrieval

--- apps/exam_public/custom/candidate_exam_classes.py
+++ apps/exam_public/custom/candidate_exam_classes.py
@@ -71,32 +71,6 @@ class CandidateExamNinja:
         pass
 
     def get_candidate_exam(self, candidate_exam_id):
-        logged_in_user_id = get_current_user().id  # type:ignore
-
-        # TODO: Remove this token logic after new exam attempt flow have been added by the front-end and remove the permission access from candidate to get the exam
-        try:
-            candidate_exam_id = int(candidate_exam_id)
-        except:
-            try:
-                token = candidate_exam_id[len("token=") :]
-                key = get_encryption_key()
-                cipher = Fernet(key)
-                decrypted_data = json.loads(cipher.decrypt(token).decode())
-                candidate_exam_id = decrypted_data["candidate_exam_id"]
-            except:
-                ResponseMiddleware.return_now(make_error_response(message="Invalid token"))
-
-        # * IF ROLES ARE ( Organization Roles and Candidate )
-        logged_in_user_roles = get_current_user().get_user_role_slugs  # type:ignore
-        if len(logged_in_user_roles):
-            if "candidate" in logged_in_user_roles:
-                candidate_exam_filter_data = {
-                    "id": candidate_exam_id,
-                    "candidate__user__id": logged_in_user_id,
-                }
-
-                if not CandidateExam.objects.filter(**candidate_exam_filter_data).exists():
-                    ResponseMiddleware.return_now(make_error_response(message="Exam not allowed to this candidate"))
         return get_detailed_candidate_exam_with_country_based_questions(candidate_exam_id)
 
     def send_exam_invitation_link(self, candidate_exam_ids: list):
